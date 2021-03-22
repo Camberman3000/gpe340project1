6595238d73c6ae954ccc6cefac0d8a3ba93d7613
Mark <camberman3000@hotmail.com>
2021-03-22 10:53:19 -0400

    Changed menu to add some settings stuff

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 using UnityEditor;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
 
 public class MenuManager : MonoBehaviour
 {
-    //[SerializeField] private Scene Main;
+    [SerializeField] private Dropdown resolution_Dropdown;
+    [SerializeField] private Toggle screenMode_Toggle;
+    [SerializeField] private Dropdown videoQuality_Dropdown;
+    private List<string> resolutionList;
+    private Resolution[] resolutions;
+    
+    private void Awake()
+    {
+        resolutions = Screen.resolutions; // Get list of screen resolutions and save to array
+        resolution_Dropdown.ClearOptions(); // Clear out existing dropdown options
+        resolutionList = new List<string>(); // Create a list to hold resolutions
+        for (int i = 0; i < Screen.resolutions.Length; i++) // Loop through each resolution
+        {
+            resolutionList.Add($"{Screen.resolutions[i].width} x {Screen.resolutions[i].height}"); // Add the resolution to the resolution list
+            resolution_Dropdown.value = i; // Set the dropdown value to the resolution index
+        }
+        resolution_Dropdown.AddOptions(resolutionList); // Add the list of resolutions to the dropdown options
+
+        
+        videoQuality_Dropdown.ClearOptions(); // Clear the video quality options
+        videoQuality_Dropdown.AddOptions(QualitySettings.names.ToList()); // Populate the video quality dropdown
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +47,12 @@ public class MenuManager : MonoBehaviour
     public void ResumeGame()
     {
         Debug.LogFormat("Clicked resume");
-        SceneManager.LoadScene("Main");
-        //GameManager.instance.Resume();
-        
+        SceneManager.LoadScene("Main");        
+    }
+
+    public void ApplySettings()
+    {
+
     }
 
     public void QuitGame()
